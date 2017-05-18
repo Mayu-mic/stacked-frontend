8fa5d9abfe690ac445f8378eacfab6c0549b0af4
Ryo Koizumi <ryo.koizumi@soinn.com>
2017-05-18 16:57:07 +0900

    無駄なアクションを整理

--- src/app/actions/user.ts
+++ src/app/actions/user.ts
@@ -2,12 +2,8 @@ import { User } from '../models/User';
 import { Action } from '@ngrx/store';
 
 export const REQUEST_LOGIN = '[User] Login';
-export const REQUEST_LOGIN_SUCCESS = '[User] Login Success';
-export const REQUEST_LOGIN_FAIL = '[User] Login Fail';
 
 export const REQUEST_LOGOUT = '[User] Logout';
-export const REQUEST_LOGOUT_SUCCESS = '[User] Logout Success';
-export const REQUEST_LOGOUT_FAIL = '[User] Logout Fail';
 
 export const REQUEST_USER_INFO = '[User] Request User Info';
 export const REQUEST_USER_INFO_SUCCESS = '[User] Request User Info Success';
@@ -17,24 +13,8 @@ export class RequestLoginAction implements Action {
     readonly type = REQUEST_LOGIN;
 }
 
-export class RequestLoginSuccessAction implements Action {
-    readonly type = REQUEST_LOGIN_SUCCESS;
-}
-
-export class RequestLoginFailAction implements Action {
-    readonly type = REQUEST_LOGIN_FAIL;
-}
-
 export class RequestLogoutAction implements Action {
     readonly type = REQUEST_LOGOUT;
-}
-
-export class RequestLogoutSuccessAction implements Action {
-    readonly type = REQUEST_LOGOUT_SUCCESS;
-}
-
-export class RequestLogoutFailAction implements Action {
-    readonly type = REQUEST_LOGOUT_FAIL;
 }
 
 export class RequestUserInfoAction implements Action {
@@ -52,11 +32,7 @@ export class RequestUserInfoFailAction implements Action {
 
 export type Actions
     = RequestLoginAction
-    | RequestLoginSuccessAction
-    | RequestLoginFailAction
     | RequestLogoutAction
-    | RequestLogoutSuccessAction
-    | RequestLogoutFailAction
     | RequestUserInfoAction
     | RequestUserInfoSuccessAction
     | RequestUserInfoFailAction
